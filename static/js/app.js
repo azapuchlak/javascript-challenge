15d203da831bbc1eddfb136f4bf237562cf3499a
Alexandra Zapuchlak <azapuchlak@gmail.com>
2021-06-09 00:07:03 -0500

    created function to render table and populate with the data file & wrote code for search button function

--- static/js/app.js
+++ static/js/app.js
@@ -14,12 +14,44 @@ var $dateInput = document.querySelector("#datetime");
 //and will execute the search button action
 $searchButton.addEventListener("click", searchButtonAction);
 
+//create function that calls the table data
+//first loading the data and then inserting each line as a new row  
+function displayTableInfo(){
+    $tbody.innerHTML = "";
+    
+    for (var i = 0; i < tableData.length; i++) {
+        //Retrieve the data
+        var data = tableData[i];
+        var fields = Object.keys(data);
+        
+        //Will add a new row to table
+        var $row = $tbody.insertRow(i);
+        for (var x = 0; x < fields.length; x++) {
+            var field = fields[x];
+            var $cell = $row.insertCell(x);
+            $cell.innerText = data[field]; 
+        }
+    }
+}
+
+
 //set the above searchButtonAction as a function to perform the search
-//function searchButtonAction (TBD)
+function searchButtonAction(event) {
+    event.preventDefault();
 
-//this will render all the table data 
-//prob use for loop
+    var filterField = $dateInput.value.trim();
+    //if the filter field is *not* (!) null then return results
+    //this is how data populates
+    if (filterField !="") {
+        tableData = data.filter(function (data) {
+            var dateField = data.datetime;
+            return dateField === filterField;
+        });
+    };
+    displayTableInfo();
+}
 
-//connect data into table
 
 //Call to the table to render
+displayTableInfo();
+console.log(displayTableInfo)
