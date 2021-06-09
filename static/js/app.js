645fab104a8858c72741da02ae392030cf6a94c7
Alexandra Zapuchlak <azapuchlak@gmail.com>
2021-06-08 23:10:47 -0500

    added in event listener

--- static/js/app.js
+++ static/js/app.js
@@ -8,3 +8,18 @@ var tableData = data;
 var $tbody = document.querySelector("tbody");
 var $searchButton = document.querySelector("#filter-btn");
 var $dateInput = document.querySelector("#datetime");
+
+//When the search button is clicked, run the search button action
+//This requires adding in an event listener - adding click makes clicking the button actionable...
+//and will execute the search button action
+$searchButton.addEventListener("click", searchButtonAction);
+
+//set the above searchButtonAction as a function to perform the search
+//function searchButtonAction (TBD)
+
+//this will render all the table data 
+//prob use for loop
+
+//connect data into table
+
+//Call to the table to render
